feb217882b9da5416986916d0bb85a1bbaf634d0
Crijosicar <crijosicar@hotmail.com>
2016-12-23 13:26:57 -0500

    Front mockup login

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -3,39 +3,41 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomeComponent } from '../pages/home/home.component';
+import { LoginComponent } from '../pages/login/login.component';
 import { Page2 } from '../pages/page2/page2';
 
 
 @Component({
-  templateUrl: 'app.html'
+    templateUrl: 'app.html'
 })
 export class MyApp {
-  @ViewChild(Nav) nav: Nav;
+    @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomeComponent;
+    rootPage: any = LoginComponent;
 
-  pages: Array<{title: string, component: any}>;
+    pages: Array<{ title: string, component: any }>;
 
-  constructor(public platform: Platform) {
-    this.initializeApp();
+    constructor(public platform: Platform) {
+        this.initializeApp();
 
-    this.pages = [
-      { title: 'Page One', component: HomeComponent },
-      { title: 'Page Two', component: Page2 }
-    ];
+        this.pages = [
+            { title: 'Login', component: LoginComponent },
+            { title: 'Page One', component: HomeComponent },
+            { title: 'Page Two', component: Page2 }
+        ];
 
-  }
+    }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      StatusBar.styleDefault();
-      Splashscreen.hide();
-    });
-  }
+    initializeApp() {
+        this.platform.ready().then(() => {
+            StatusBar.styleLightContent();
+            Splashscreen.hide();
+        });
+    }
 
-  openPage(page) {
-    this.nav.setRoot(page.component);
-  }
+    openPage(page) {
+        this.nav.setRoot(page.component);
+    }
 }
 
 
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+
 import { MyApp } from './app.component';
+import { LoginComponent } from '../pages/login/login.component';
 import { HomeComponent } from '../pages/home/home.component';
 import { Page2 } from '../pages/page2/page2';
 
 @NgModule({
   declarations: [
     MyApp,
+    LoginComponent,
     HomeComponent,
     Page2
   ],
@@ -16,6 +19,7 @@ import { Page2 } from '../pages/page2/page2';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    LoginComponent,
     HomeComponent,
     Page2
   ],
