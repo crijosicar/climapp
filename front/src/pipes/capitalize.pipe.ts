b6f67cfddb83a3ab51c87cd815c15850013f48a6
crijosicar <crijosicar@hotmail.com>
2017-02-02 16:42:10 -0500

    fix

--- front/src/pipes/capitalize.pipe.ts
+++ front/src/pipes/capitalize.pipe.ts
@@ -6,6 +6,7 @@ export class CapitalizePipe implements PipeTransform {
 
     transform(value:any) {
         if (value) {
+            value = value.toLowerCase();
             return value.charAt(0).toUpperCase() + value.slice(1);
         }
         return value;
