21a74088b5f7d49e25e106bdef681f60245524a1
Crijosicar <crijosicar@hotmail.com>
2017-05-01 16:37:05 -0500

    Fix

--- front/src/common/const-util.ts
+++ front/src/common/const-util.ts
@@ -2,7 +2,7 @@ export const URL_API = "http://climapprepository.dev/api/v1/";
 
 export const MONTHS_LARGE_NAMES = ['enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembre','diciembre'];
 
-export const MONTHS_SHORT_NAMES = ['enero','feb','marzo','abr','mayo','jun','jul','agosto','sept','oct','nov','dic'];
+export const MONTHS_SHORT_NAMES = ['ener','feb','marz','abr','mayo','jun','jul','agos','sept','oct','nov','dic'];
 
 export const PATTERN_EMAIL = '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$';
 
